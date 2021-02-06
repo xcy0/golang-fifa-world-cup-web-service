5438b68867e6c14e89f605b3ab161b0c3c623fca
xcy0 <xuchy@hotmail.com>
2021-02-06 10:29:04 +0800

    dispatcher

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -51,5 +51,13 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 
 // WinnersHandler is the dispatcher for all /winners URL
 func WinnersHandler(res http.ResponseWriter, req *http.Request) {
-
+	switch req.Method {
+	case http.MethodGet:
+		ListWinners(res, req)
+	case http.MethodPost:
+		AddNewWinner(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 }
